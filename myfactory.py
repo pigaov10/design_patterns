a9204aa116ea19d98c22f13656de8eef7606f2aa
Raphael Iarussi <pigaov10@gmail.com>
2017-08-07 16:17:42 -0300

    add factory

---
+++ myfactory.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+
+class Area(object):
+    TYPE = None
+
+    def __init__(self, largura, comprimento):
+        self.largura = largura
+        self.comprimento = comprimento
+
+    def calcular(self):
+        raise NotImplementedError()
+
+    @classmethod
+    def checar_calculo(cls, tipo_calculo):
+        return tipo_calculo == cls.TYPE
+
+class Quadrado(Area):
+    TYPE = 'Quadrado'
+
+    def calcular(self):
+        return self.largura * self.comprimento
+
+class TrianguloRetangulo(Area):
+    TYPE = 'TrianguloRetangulo'
+
+    def calcular(self):
+        return 'Pitagoras c2 = a2 + b2'
+
+
+# factory
+class AreaFactory(object):
+    TIPOS_CALCULO = [Quadrado, TrianguloRetangulo]
+    def __init__(self, tipo, largura, comprimento):
+        self.tipo = tipo
+        self.largura = largura
+        self.comprimento = comprimento
+
+    def rodar(self):
+        for obj in self.TIPOS_CALCULO:
+            if obj.checar_calculo(self.tipo):
+                return obj(self.largura, self.comprimento).calcular()
+
+if __name__ == '__main__':
+    calculo = AreaFactory('Quadrado', 5, 5)
+    print(calculo.rodar())
